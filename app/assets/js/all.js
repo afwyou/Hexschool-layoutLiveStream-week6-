64574cc7100a2f51789b552c3589d716901e6b85
afwyou <afwyousenge@gmail.com>
2022-08-14 23:06:58 +0800

    why header menu can't  hide

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,12 @@
-$(function() {
+$(function () {
   console.log('Hello Bootstrap5');
+});
+
+
+$(document).ready(function () {
+  //點擊
+  $('.menu-icon').on('click', function (e) {
+    e.preventDefault();
+    $('header').toggleClass('show-menu')
+  })
 });
