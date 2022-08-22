eefb5b872173c0cb03a212c762ff23d77676ae5c
afwyou <afwyousenge@gmail.com>
2022-08-22 16:06:55 +0800

    Update 2022-08-22T08:06:50.815Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,43 +1,36 @@
+"use strict";
+
 $(function () {
   console.log('Hello Bootstrap5');
 });
-
-
 $(document).ready(function () {
   //點擊
   $('.menu-icon').on('click', function (e) {
     e.preventDefault();
-    $('header').toggleClass('show-menu')
-  })
+    $('header').toggleClass('show-menu');
+  });
 });
-
-
-const swiper = new Swiper('.swiper', {
+var swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
   effect: "fade",
-
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    type: 'bullets',
+    type: 'bullets'
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-prev'
   },
-
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+    el: '.swiper-scrollbar'
+  }
 });
-
-
-const elem = document.querySelector('input[name="foo"]');
-const datepicker = new Datepicker(elem, {
-  // ...options
-}); 
+var elem = document.querySelector('input[name="foo"]');
+var datepicker = new Datepicker(elem, {// ...options
+});
+//# sourceMappingURL=all.js.map
